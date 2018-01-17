24828e49106c52d7988d6a55fdf7d7cdf4b55565
washicjr <washicjr@gmail.com>
2018-01-17 14:02:11 -0600

    fixed error with commented line 297

--- reports.ps1
+++ reports.ps1
@@ -294,7 +294,7 @@ function exportUserMapping {
 #%%%%%%%%%%%%%%%%%%%%%%%%%%  Script Execution %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-#importData
+importData
 exportDecisions
 exportDevonContacts
 exportRequests
